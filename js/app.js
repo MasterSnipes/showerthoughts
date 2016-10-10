539d8dc6d48c7b3fc7170964d427780e9878997f
BuildTools <unconfigured@null.spigotmc.org>
2016-10-09 22:49:54 -0400

    s'more stuff

--- js/app.js
+++ js/app.js
@@ -4,18 +4,9 @@ $.getJSON(
   "https://www.reddit.com/r/showerthoughts.json",
   function foo(data)
   {
-    // $.each(
-    //   data.data.children,
-    //   function (i, post) {
-    //     if(!post.data.stickied){
-
-    //     $("#reddit-content").append( '<hr>' + post.data.title );
-    //   }
-    //   }
-    // )
     var postNum = Math.floor(Math.random() * data.data.children.length);
     if(!data.data.children[postNum].data.stickied){
-	$('#reddit-content').html('<a href="'+ data.data.children[postNum].data.url + '"> ' + data.data.children[postNum].data.title + "</a>");
+	$('#reddit-content').html('<h4><a href="'+ data.data.children[postNum].data.url + '"> ' + data.data.children[postNum].data.title + "</a></h4>");
 	}
   }
 );
