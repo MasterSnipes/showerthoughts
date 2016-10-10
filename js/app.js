7def9d69c3f6bae53948fedccf4d67fc9131b2cb
BuildTools <unconfigured@null.spigotmc.org>
2016-10-10 16:48:27 -0400

    no nsfw here folks

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $.getJSON(
   function foo(data)
   {
     var postNum = Math.floor(Math.random() * data.data.children.length);
-    if(!data.data.children[postNum].data.stickied){
+    if(!data.data.children[postNum].data.stickied && !data.data.children[postNum].data.over_18){
 	$('#reddit-content').html('<h4><a href="'+ data.data.children[postNum].data.url + '"> ' + data.data.children[postNum].data.title + "</a></h4>");
 	}
   }
